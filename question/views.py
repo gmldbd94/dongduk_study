35f1c57301b160e61a2a486f88e65845f63b1691
bjy1530 <bjy1530@naver.com>
2019-03-03 19:11:46 +0900

    add global

--- question/views.py
+++ question/views.py
@@ -1,11 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 import getpass # 현재 사용중인 유저 반환
 from .models import Questions, Post
 import random
 from django.contrib.auth.models import User
 from django.contrib import auth
+from django.utils import timezone 
 # Create your views here.
 
+global question
+global all
+all=Questions.objects.all()
+question=random.choice(all)
 
 def main(request):
     return render(request, 'question/main.html')
@@ -17,10 +22,20 @@ def post(request):
     # current user 받아오기
 
     # Question 객체 중 랜덤 반환
-    all=Questions.objects.all()
-    question=random.choice(all)
+    # all=Questions.objects.all()
+    # question=random.choice(all)
     return render(request, 'question/post.html', {'question':question})
 
+
+def create(request):
+    post=Post() 
+    post.pub_date=timezone.datetime.now()
+    post.answer=request.GET['answer']
+    post.author=request.GET['author']
+    post.question=question
+    post.save()
+    # 객체.delete()는 이 데이터객체를 지워라
+    return redirect('/question/main')
 
 def signup(request):
     if request.method == 'POST':
@@ -28,7 +43,7 @@ def signup(request):
             user = User.objects.create_user(
             request.POST['username'], password=request.POST['password1'])
             auth.login(request, user)
-            return redirect('/')
+            return redirect('/question/main')
     return render(request, 'question/signup.html')
 
 
@@ -39,7 +54,7 @@ def login(request):
         user = auth.authenticate(request, username=username, password=password)
         if user is not None:
             auth.login(request, user)
-            return redirect('/')
+            return redirect('question/main.html')
         else:
             return render(request, 'question/login.html', {'error': 'username or password is incorrect.'})
     else:
